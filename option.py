5f398a4ac3510aa16a6ddb8438e530860d32b04f
quyip <quyip@hotmail.com>
2025-02-08 21:54:18 -0800

    fix

--- option.py
+++ option.py
@@ -31,10 +31,11 @@ def process_option(df_raw, current_price, is_call):
 
 
 def max_time_value(df, current_price):
-    df2 = df[['strike', 'timeValue']].copy()
+    df2 = df[['strike', 'timeValue', 'impliedVolatility']].copy()
     df2['price_diff'] = abs(df2['strike'] - current_price)
     df2 = df2.sort_values(['price_diff'], ascending=[True]).head(3)
-    return df2['timeValue'].max()
+    df2 = df2.sort_values(['timeValue'], ascending=[False]).head(1)
+    return float(df2['timeValue'].item()), float(df2['impliedVolatility'].item())
 
 
 def process_max_time_value_df(df_raw, current_price):
@@ -48,23 +49,32 @@ def process_max_time_value_df(df_raw, current_price):
     df['dayValPct'] = df['dayValuePercent'].apply(lambda x: f"{x:.2%}")
     df['yearValPct'] = df['yearValuePercent'].apply(lambda x: f"{x:.2%}")
     df['val'] = df['value'].apply(lambda x: f"{x:.3f}")
+    df['IV'] = df['impliedVolatility'].apply(lambda x: f"{x:.3f}")
     df['x'] = ''
     df = df[
-        ['days', 'valPct', 'dayValPct', 'yearValPct', 'val', 'x', 'valuePercent', 'dayValuePercent', 'yearValuePercent',
-         'value']]
+        ['days', 'valPct', 'dayValPct', 'yearValPct', 'val', 'IV', 'x', 'valuePercent', 'dayValuePercent',
+         'yearValuePercent', 'value', 'impliedVolatility']]
 
     if (df['days'] == 7).any():
-        week_value = df[df['days'] == 7]['value']
+        week_row = df[df['days'] == 7].iloc[0]
     elif (df['days'] == 6).any():
-        week_value = df[df['days'] == 6]['value']
+        week_row = df[df['days'] == 6].iloc[0]
     elif (df['days'] == 5).any():
-        week_value = df[df['days'] == 5]['value']
+        week_row = df[df['days'] == 5].iloc[0]
     else:
-        week_value = df[df['days'] == 4]['value']
+        week_row = df[df['days'] == 4].iloc[0]
 
-    max_day_value = df.loc[df['days'].idxmax()]['value']
+    max_day_row = df.loc[df['days'].idxmax()]
+    payback_weeks = max_day_row['value'] / week_row['value'] if max_day_row['value'].any() and week_row[
+        'value'].any() else ''
 
-    return df.sort_values(by="days"), float(max_day_value.item()) / float(week_value.item()) if max_day_value.any() and week_value.any() else ''
+    return df.sort_values(by="days"), [payback_weeks, week_row['valPct'], max_day_row['valPct']]
+
+
+def save_header_data(writer, sheet_name, data):
+    pd.DataFrame.from_dict(data, orient="index").to_excel(writer, sheet_name=sheet_name, index=True, header=False,
+                                                          startrow=0)
+    return len(data) + 1
 
 
 def save_option_data(symbol, today):
@@ -73,8 +83,8 @@ def save_option_data(symbol, today):
 
     call_dfs = []
     put_dfs = []
-    call_max_time_value_df = pd.DataFrame(columns=["days", "value"])
-    put_max_time_value_df = pd.DataFrame(columns=["days", "value"])
+    call_max_time_value_df = pd.DataFrame(columns=["days", "value", "impliedVolatility"])
+    put_max_time_value_df = pd.DataFrame(columns=["days", "value", "impliedVolatility"])
 
     for date_str in ticker.options:
         opt_chain = ticker.option_chain(date_str)
@@ -88,26 +98,26 @@ def save_option_data(symbol, today):
         call_dfs.append([call_df, f"c_{diff_days}"])
         put_dfs.append([put_df, f"p_{diff_days}"])
 
-        call_max_time_value_df.loc[len(call_max_time_value_df)] = [diff_days, max_time_value(call_df, current_price)]
-        put_max_time_value_df.loc[len(put_max_time_value_df)] = [diff_days, max_time_value(put_df, current_price)]
+        call_max_time_value_df.loc[len(call_max_time_value_df)] = [diff_days, *max_time_value(call_df, current_price)]
+        put_max_time_value_df.loc[len(put_max_time_value_df)] = [diff_days, *max_time_value(put_df, current_price)]
 
     today_str = today.strftime("%Y_%m_%d")
     os.makedirs(f"options/{today_str}", exist_ok=True)
 
     with pd.ExcelWriter(f"options/{today_str}/{symbol}_{today_str}.xlsx") as writer:
-        call_max_time_value_df, payback_weeks = process_max_time_value_df(call_max_time_value_df, current_price)
-        pd.DataFrame({
-            'rowName': ['currentPrice', 'payback_weeks'],
-            'rowValue': [current_price, payback_weeks],
-        }).to_excel(writer, sheet_name='c_all', index=False, header=False, startrow=0)
-        call_max_time_value_df.to_excel(writer, sheet_name='c_all', index=False, startrow=3)
+        call_max_time_value_df, paybacks = process_max_time_value_df(call_max_time_value_df, current_price)
+        next_start_row = save_header_data(writer, "c_all", {
+            "currentPrice": [current_price],
+            "paybacks": paybacks
+        })
+        call_max_time_value_df.to_excel(writer, sheet_name='c_all', index=False, startrow=next_start_row)
 
-        put_max_time_value_df, payback_weeks = process_max_time_value_df(put_max_time_value_df, current_price)
-        pd.DataFrame({
-            'rowName': ['currentPrice', 'payback_weeks'],
-            'rowValue': [current_price, payback_weeks],
-        }).to_excel(writer, sheet_name='p_all', index=False, header=False, startrow=0)
-        put_max_time_value_df.to_excel(writer, sheet_name='p_all', index=False, startrow=3)
+        put_max_time_value_df, paybacks = process_max_time_value_df(put_max_time_value_df, current_price)
+        next_start_row = save_header_data(writer, "p_all", {
+            "currentPrice": [current_price],
+            "paybacks": paybacks
+        })
+        put_max_time_value_df.to_excel(writer, sheet_name='p_all', index=False, startrow=next_start_row)
 
         current_price_df = pd.DataFrame({
             'rowName': ['currentPrice'],
