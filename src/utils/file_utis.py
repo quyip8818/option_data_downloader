a7848567c804e24d552ca24bec9cb147dc6b6f69
quyip <quyip@hotmail.com>
2025-03-18 22:22:35 -0400

    fix

---
+++ src/utils/file_utis.py
@@ -0,0 +1,8 @@
+import subprocess
+import platform
+
+def open_file_in_application(path):
+    if platform.system() == 'Darwin':  # macOS
+        subprocess.run(['open', path])
+    else:  # Linux
+        subprocess.run(['xdg-open', path])
---
+++ view.py
@@ -0,0 +1,10 @@
+from src.utils.file_utis import open_file_in_application
+from src.utils.path_utils import get_quandl_path
+
+
+def read_iv_rank(symbol):
+    open_file_in_application(get_quandl_path(f"option_iv_rank_by_symbols/{symbol}.csv"))
+
+
+if __name__ == "__main__":
+    read_iv_rank("IOT")
