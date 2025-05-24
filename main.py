703cd3dc40d9950dd9f6079ad9b164e7ddb5bc92
quyip <quyip@hotmail.com>
2025-05-25 01:37:09 +0800

    fix

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import datetime
 
-from src.quandl.option_percentiles import get_last_iv_rank, fetch_option_percentiles
+from src.quandl.option_percentiles import fetch_option_percentiles
 from src.utils.date_utils import get_last_workday
-from src.yfinance.all_options import fetch_all_yf_options
 
-# today = datetime.date(2025, 5, 15)
-today = get_last_workday(datetime.date.today())
+today = datetime.date(2025, 5, 23)
+# today = get_last_workday(datetime.date.today())
 
 SKIP_SYMBOLS = {}
 
